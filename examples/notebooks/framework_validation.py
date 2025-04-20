e905388df179e706d6d6a13d8d22d87011df3ab6
Chris Marshall <chris.marshall@optimapartners.co.uk>
2025-04-20 16:17:55 +0100

    Fix

--- examples/notebooks/framework_validation.py
+++ examples/notebooks/framework_validation.py
@@ -1,12 +1,11 @@
-# MAGIC %pip install pyspark pyyaml
-
-# COMMAND ----------
-
 # Databricks notebook source
 # MAGIC %md
 # MAGIC # DLT Medallion Framework Validation
 # MAGIC 
 # MAGIC This notebook demonstrates the usage of the DLT Medallion Framework with both YAML and object-based configuration.
+
+# COMMAND ----------
+# MAGIC %pip install pyspark pyyaml
 
 # COMMAND ----------
 
